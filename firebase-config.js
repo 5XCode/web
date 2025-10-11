7610dc27bcab024865d9c87b33a58897be77b610
KIMO PRO <lava4ggaming@gmail.com>
2025-10-11 19:06:02 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,12 @@
-
-// إعدادات Firebase
+// إعدادات Firebase لمشروعك
 const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-project.firebaseapp.com",
-  databaseURL: "https://your-project-default-rtdb.firebaseio.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "your-app-id"
+  apiKey: "AIzaSyBzB4Hky2iO8FGe_sRz3EfjAfyy4ClgLGQ",
+  authDomain: "marvel-75d8b.firebaseapp.com",
+  databaseURL: "https://marvel-75d8b-default-rtdb.firebaseio.com",
+  projectId: "marvel-75d8b",
+  storageBucket: "marvel-75d8b.firebasestorage.app",
+  messagingSenderId: "1098663412275",
+  appId: "1:1098663412275:web:your-web-app-id" // تحتاج لإضافة تطبيق ويب
 };
 
 // تهيئة Firebase
